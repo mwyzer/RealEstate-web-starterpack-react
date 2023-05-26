1081dc2f35af5b3d3904e32b1d6751e4b3263b5f
mwyzer <muhammad.wyzer@gmail.com>
2023-05-27 01:24:55 +0700

    search bar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Hero.css';
+import { HiLocationMarker } from 'react-icons/hi';
+const Hero = () => {
+  return (
+    <section className='hero-wrapper'>
+      <div className='paddings innerWidth hero-container flexCenter'>
+        {/* left side */}
+        <div className='flexColStart hero-left'>
+          <div className='hero-title'>
+            <div className='orange-circle' />
+            <h1>
+              Discover <br /> Most Suitable <br /> Property
+            </h1>
+          </div>
+
+          <div className='flexColStart hero-des'>
+            <span>Find a variety of properties that suit you very easily</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className='flexCenter search-bar'>
+            <HiLocationMarker color='var(--blue)' size={25} />
+            <input type='text' />
+            <button className='button'>Search</button>
+          </div>
+        </div>
+        {/* right side */}
+        <div className='flexCenter hero-right'>
+          <div className='image-container'>
+            <img src='./hero-image.png' alt='' />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
